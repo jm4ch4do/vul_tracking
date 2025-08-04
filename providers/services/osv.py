9c70c34ff0e61b57101c30d37863ccab2f678923
jose <m4ch4do@protonmail.com>
2025-08-03 22:38:04 -0400

    feat: adds caching to vul queries
    Cache was introduce inside OSVScanner to make it simple but ideally it
    would be referenced at providers/cache/vul_cache.py with a separate
    class.
    
    TTL was set to 10 seconds for testing purposes but it should be
    generally around 3600(1h)
    
    Behave test demonstrating cache usage needs to be added.

--- providers/services/osv.py
+++ providers/services/osv.py
@@ -9,11 +9,18 @@ import domain.vul as _d_vul
 
 class OSVScanner:
     API_URL = "https://api.osv.dev/v1/querybatch"
+    _cache: _t.Dict[str, _t.Tuple[_t.List[_d_vul.Vul], _dt.datetime]] = {}
+    _TTL_SECONDS = 10
 
     def __call__(
         self, dependencies: _t.List[_d_dep.Dependency]
     ) -> _t.Dict[str, _t.List[_d_vul.Vul]]:
         """Queries osv API for vulnerabilities in the provided 'dependencies' list."""
+
+        cached, to_query = self._get_cached_or_new_results(dependencies)
+
+        if not to_query:
+            return cached
 
         payload = {
             "queries": [
@@ -21,7 +28,7 @@ class OSVScanner:
                     "package": {"name": dep.name, "ecosystem": "PyPI"},
                     "version": dep.version,
                 }
-                for dep in dependencies
+                for dep in to_query
             ]
         }
 
@@ -30,7 +37,42 @@ class OSVScanner:
             raise Exception(f"OSV API failed: {response.status_code} {response.text}")
 
         raw_results = response.json().get("results", [])
-        return self._format_results(dependencies, raw_results)
+        new_results = self._format_results(to_query, raw_results)
+
+        self._update_cache(new_results)
+
+        combined = {**cached, **new_results}
+        return combined
+
+    def _get_cached_or_new_results(
+        self, dependencies: _t.List[_d_dep.Dependency]
+    ) -> _t.Tuple[
+        _t.Dict[str, _t.List[_d_vul.Vul]],
+        _t.List[_d_dep.Dependency],
+    ]:
+        cached = {}
+        to_query = []
+        now = _dt.datetime.now(_dt.timezone.utc)
+
+        for dep in dependencies:
+            cache_entry = self._cache.get(dep.id)
+            if cache_entry:
+                vulns, cached_time = cache_entry
+                elapsed = (now - cached_time).total_seconds()
+                if elapsed <= self._TTL_SECONDS:
+                    cached[dep.id] = vulns
+                    continue  # cache valid
+                else:
+                    del self._cache[dep.id]  # expired, remove cache entry
+
+            to_query.append(dep)
+
+        return cached, to_query
+
+    def _update_cache(self, new_results: _t.Dict[str, _t.List[_d_vul.Vul]]) -> None:
+        now = _dt.datetime.now(_dt.timezone.utc)
+        for dep_id, vulns in new_results.items():
+            self._cache[dep_id] = (vulns, now)
 
     def _format_results(
         self,
